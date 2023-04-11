49305bd2f26550edfd30a7d5b91e6109f0f1d592
KubishinMikhail <kubishin.m@gmail.com>
2023-04-11 17:36:13 +0300

    start project

---
+++ main.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template, request, redirect, make_response, session, flash
+from forms.loginform import LoginForm
+from forms.user import RegisterForm
+from flask_login import LoginManager
+from flask_login import login_user, login_required, logout_user, current_user
+
+from data import db_session
+from data.news import News
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "my mega secret key"
+
+USERS = {"Alex": "123456",
+         "Boris": "qwerty",
+         "Albert": "LLlka0l0"}
+user = ''
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    if current_user.is_authenticated:
+        news = db_sess.query(News).filter(
+            (News.user == current_user) | (News.is_private != True))
+    else:
+        news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route("/sign_in", methods=['GET', 'POST'])
+def sing_in():
+    form = LoginForm()
+    global user
+    if form.validate_on_submit():
+        if form.username.data in USERS:
+            if form.password.data == USERS[form.username.data]:
+                user = form.username.data
+                return redirect("/lms")
+        flash(f'Ошибка авторизации!')
+    return render_template("sign_in.html", form=form)
+
+
+@app.route("/lms")
+def lms():
+    data = {"username": user, "title": "Личный кабинет"}
+    return render_template("lms.html", **data)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route("/cookie_test")
+def cookie_test():
+    visits_count = int(request.cookies.get("visits_count", 0))
+    if visits_count:
+        res = make_response(
+            f"Вы пришли на эту страницу {visits_count + 1} раз")
+        res.set_cookie("visits_count", str(visits_count + 1),
+                       max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response(
+            "Вы пришли на эту страницу в первый раз за последние 2 года")
+        res.set_cookie("visits_count", '1',
+                       max_age=60 * 60 * 24 * 365 * 2)
+    return res
+
+
+@app.route("/session_test")
+def session_test():
+    visits_count = session.get('visits_count', 0)
+    session['visits_count'] = visits_count + 1
+    return make_response(
+        f"Вы пришли на эту страницу {visits_count + 1} раз")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    db_session.global_init("db/db_flask.db")
+
+    db_sess = db_session.create_session()
+
+    app.run(debug=True)
